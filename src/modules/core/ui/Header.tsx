55b7cc14989b73a7df5ba5fffbe3f4b03fbf5e32
BastienCorre <bcorre@millesima.com>
2024-03-08 08:57:14 +0100

    refactor: move logo to public folder

--- src/modules/core/ui/Header.tsx
+++ src/modules/core/ui/Header.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import logo from '../../../assets/images/logo.png';
 
 import styles from './Header.module.scss';
 
@@ -8,7 +7,7 @@ export const Header = () => {
     return (
         <header className={styles.container}>
             <div className={styles.logo}>
-                <Image src={logo} alt="logo" width={160} height={50} />
+                <Image src={"/logo.png"} alt="logo" width={160} height={50} />
             </div>
             <div className={styles.navbar}>
                 <a className={styles.library}>Bibliothèque</a>
