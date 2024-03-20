886efe80e5e9eab2783d53dd69ebb8fa9babc786
laurabjn <92058813+laurabjn@users.noreply.github.com>
2024-03-20 22:05:43 +0100

    feat: init i18next for internationalization

--- src/modules/ui/Footer.tsx
+++ src/modules/ui/Footer.tsx
@@ -1,11 +1,13 @@
+import { useTranslation } from 'react-i18next';
 import styles from './Footer.module.scss';
 
 export const Footer = () => {
+    const { t } = useTranslation('footer');
     return (
         <footer className={styles.container}>
             <div className={styles.footerContent}>
-                <a className={styles.donation} href="#">Faire un don</a>
-                <a className={styles.contact} href="#">Nous Contacter</a>
+                <a className={styles.donation} href="#">{t('donation')}</a>
+                <a className={styles.contact} href="#">{t('contact')}</a>
             </div>
         </footer>
     )
--- src/modules/ui/Header.tsx
+++ src/modules/ui/Header.tsx
@@ -1,17 +1,19 @@
 import Image from 'next/image';
+import { useTranslation } from 'react-i18next';
 
 import styles from './Header.module.scss';
 
 
 export const Header = () => {
+    const { t } = useTranslation('navbar');
     return (
         <header className={styles.container}>
             <div className={styles.logo}>
                 <Image src={"/logo.png"} alt="logo" width={160} height={50} />
             </div>
             <div className={styles.navbar}>
-                <a className={styles.library}>Bibliothèque</a>
-                <a className={styles.login}>Se Connecter</a>
+                <a className={styles.library}>{t('library')}</a>
+                <a className={styles.login}>{t('login')}</a>
             </div>
         </header>
     )
