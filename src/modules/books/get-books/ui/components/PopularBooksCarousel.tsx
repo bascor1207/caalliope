9814081eafbabfb10331fe9fb3e8b0cf802ca91a
laurabjn <92058813+laurabjn@users.noreply.github.com>
2024-03-21 22:27:53 +0100

    refactor: slider library and css

--- src/modules/books/get-books/ui/components/PopularBooksCarousel.tsx
+++ src/modules/books/get-books/ui/components/PopularBooksCarousel.tsx
@@ -1,12 +1,23 @@
 'use client'
-import styles from "@/modules/books/get-books/ui/components/books-carousels.module.scss";
-import { ReactNode } from 'react';
-import { useSelector } from 'react-redux';
-import { Carousel } from "react-responsive-carousel";
+import { ReactNode, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Swiper } from 'swiper/react';
+import { FreeMode, Pagination } from 'swiper/modules';
 import { BookCarouselCard } from "./BookCarouselCard";
 import { getPopularBooksViewmodel } from "../popular-books-carousel/get-popular-books.viewmodel";
+import { getPopularBooksUseCase } from "../../usecase/get-popular-books/get-popular-books.usecase";
+import { AppDispatch } from "@/modules/store/create-store";
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import styles from "@/modules/books/get-books/ui/components/books-carousels.module.scss";
 
 export const PopularBooksCarousels = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  useEffect(() => {
+    dispatch(getPopularBooksUseCase);
+  }, []);
+  
   const viewmodel = useSelector(getPopularBooksViewmodel());
 
   const nodeToRender: ReactNode = (() => {
@@ -18,17 +29,25 @@ export const PopularBooksCarousels = () => {
           case 'gettingPopularBooksFulfilled':
               return (
                   <div className={styles.carousel}>
-                    <Carousel>
-                    {viewmodel.books.map((book) => {
-                        console.log(book)
-                        return (
-                          <BookCarouselCard
-                            key={book.id}
-                            book={book}
-                          />
-                        );
-                      })}
-                    </Carousel>
+                    <Swiper
+                      slidesPerView={3}
+                      spaceBetween={20}
+                      freeMode={true}
+                      pagination={{
+                        clickable: true,
+                      }}
+                      modules={[FreeMode, Pagination]}
+                      className="mySwiper">
+                      {viewmodel.books.map((book) => {
+                          console.log(book)
+                          return (
+                            <BookCarouselCard
+                              key={book.id}
+                              book={book}
+                            />
+                          );
+                        })}
+                    </Swiper>
                   </div>
               );
       }
--- src/modules/catalog.ts
+++ src/modules/catalog.ts
@@ -1,5 +1,7 @@
 export const catalog = [
   {
+    'id': 1,
+    'title': 'book title',
     'author': 'John Doe',
     'type': 'Fiction',
     'subject': 'Mystery',
@@ -7,6 +9,8 @@ export const catalog = [
     'image': '/livre1.jpg'
   },
   {
+    'id': 2,
+    'title': 'book title',
     'author': 'Jane Smith',
     'type': 'Non-Fiction',
     'subject': 'Science',
@@ -14,6 +18,8 @@ export const catalog = [
     'image': '/livre1.jpg'
   },
   {
+    'id': 3,
+    'title': 'book title',
     'author': 'Robert Johnson',
     'type': 'Fantasy',
     'subject': 'Adventure',
@@ -21,60 +27,8 @@ export const catalog = [
     'image': '/livre1.jpg'
   },
   {
-    'author': 'Alice Williams',
-    'type': 'Historical Fiction',
-    'subject': 'History',
-    'dateOfPublication': '2019-05-20',
-    'image': '/livre1.jpg'
-  },  {
-    'author': 'John Doe',
-    'type': 'Fiction',
-    'subject': 'Mystery',
-    'dateOfPublication': '2022-03-15',
-    'image': '/livre1.jpg'
-  },
-  {
-    'author': 'Jane Smith',
-    'type': 'Non-Fiction',
-    'subject': 'Science',
-    'dateOfPublication': '2021-07-10',
-    'image': '/livre1.jpg'
-  },
-  {
-    'author': 'Robert Johnson',
-    'type': 'Fantasy',
-    'subject': 'Adventure',
-    'dateOfPublication': '2020-12-05',
-    'image': '/livre1.jpg'
-  },
-  {
-    'author': 'Alice Williams',
-    'type': 'Historical Fiction',
-    'subject': 'History',
-    'dateOfPublication': '2019-05-20',
-    'image': '/livre1.jpg'
-  },  {
-    'author': 'John Doe',
-    'type': 'Fiction',
-    'subject': 'Mystery',
-    'dateOfPublication': '2022-03-15',
-    'image': '/livre1.jpg'
-  },
-  {
-    'author': 'Jane Smith',
-    'type': 'Non-Fiction',
-    'subject': 'Science',
-    'dateOfPublication': '2021-07-10',
-    'image': '/livre1.jpg'
-  },
-  {
-    'author': 'Robert Johnson',
-    'type': 'Fantasy',
-    'subject': 'Adventure',
-    'dateOfPublication': '2020-12-05',
-    'image': '/livre1.jpg'
-  },
-  {
+    'id': 4,
+    'title': 'book title',
     'author': 'Alice Williams',
     'type': 'Historical Fiction',
     'subject': 'History',
