e0aa253d88c85e8acb8ab5d7484301fe1a289e99
laurabjn <92058813+laurabjn@users.noreply.github.com>
2024-03-28 16:20:09 +0100

    feat: init i18next in app + select language in header

--- src/modules/ui/Header.tsx
+++ src/modules/ui/Header.tsx
@@ -5,7 +5,13 @@ import styles from './Header.module.scss';
 
 
 export const Header = () => {
-    const { t } = useTranslation('navbar');
+    const { t, i18n } = useTranslation('navbar');
+    
+    const changeLanguage = (e: any) => {
+        const selectedLanguage = e.target.value;
+        i18n.changeLanguage(selectedLanguage);
+    };
+
     return (
         <header className={styles.container}>
             <div className={styles.logo}>
@@ -14,6 +20,10 @@ export const Header = () => {
             <div className={styles.navbar}>
                 <a className={styles.library}>{t('library')}</a>
                 <a className={styles.login}>{t('login')}</a>
+                <select className={styles.language} onChange={changeLanguage} value={i18n.language}>
+                    <option value="en">English</option>
+                    <option value="fr">Français</option>
+                </select>
             </div>
         </header>
     )
