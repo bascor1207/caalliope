935e1207d017a77d667906dc982dc68e6ba616d0
laurabjn <92058813+laurabjn@users.noreply.github.com>
2024-04-14 23:17:08 +0200

    feat : card book catalog

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,24 +1,59 @@
 'use client'
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-
+import { ReactNode, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { getBooksUseCase } from '@/modules/books/get-books/usecase/get-books.usecase';
 import { AppDispatch } from '@/modules/store/create-store';
-import { BooksCatalog } from '@/modules/books/get-books/ui/components/BooksCatalog';
+import { BooksListBySubject } from '@/modules/books/get-books/ui/components/books-list-by-subject';
+import { BookCard } from '@/modules/books/get-books/ui/components/BookCard';
+import { SearchLayout } from '@/modules/books/get-books/ui/components/search-layout';
+import { getBooksByNameViewmodel } from '@/modules/books/get-books/ui/get-books/get-books-by-name.viewmodel';
 
 const CatalogPage = () => {
     const dispatch = useDispatch<AppDispatch>();
+    const [subject, setSubject] = useState('');
+    const [query, setQuery] = useState('');
 
     useEffect(() => {
         dispatch(getBooksUseCase(/*{ connectedUser: true }*/))
     }, []);
 
-    return (
-        <div>
-            <BooksCatalog />
-        </div>
-    )
+    const viewmodel = useSelector(getBooksByNameViewmodel(query));
+
+    
+    const getsubject = (categ: string) => {
+        if (categ === subject) {
+        return setSubject('');
+        }
+        setSubject(categ);
+    };
+
+    const getQuery = (query: string) => {
+        setQuery(() => query);
+    };
+
+    const renderedNode: ReactNode = (() => {
+        return viewmodel.map((book) => (
+          <BookCard
+            key={book.id}
+            book={book}
+          />
+        ));
+      })();
+    
+      if (query !== '') {
+        return (
+          <SearchLayout getCategory={getsubject} query={query} getQuery={getQuery}>
+            {renderedNode}
+          </SearchLayout>
+        );
+      }
+    
+      return (
+        <SearchLayout getQuery={getQuery} query={query} getCategory={getsubject}>
+          <BooksListBySubject subject={subject} />
+        </SearchLayout>
+      );
 }
 
 export default CatalogPage;
---
+++ src/modules/books/get-books/ui/components/book-card-catalog.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import Image from 'next/image';
+import { Book } from '../../connector-to.get-books';
+
+import styles from './book-card-catalog.module.scss';
+
+type Props = {
+    book: Book;
+}
+
+export const ProductCard: React.FC<Props> = ({ book }) => {
+
+  return (
+    <div className={styles.card}>
+      <div className={styles['img-top-container']}>
+            <Image className={styles['book-image']} src={book.image} alt={'livre'} width={300} height={400}/>
+      </div>
+      <div className={styles['info-book']}>
+        <div>
+          <p className={styles['card-title']}>{book.title}</p>
+          <p className={styles['card-author']}>
+            {book.author.firstname} {book.author.lastname}
+          </p>
+        </div>
+        <p className={styles['card-subject']}>{book.subject.subject}</p>
+      </div>
+    </div>
+  );
+};
