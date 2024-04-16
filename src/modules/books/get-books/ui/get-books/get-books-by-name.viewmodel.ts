602acd075b0a86e062582c5f129f362013c5b7b9
laurabjn <92058813+laurabjn@users.noreply.github.com>
2024-04-16 22:10:02 +0200

    fix : page view model

--- src/modules/books/get-books/ui/get-books/get-books-by-name.viewmodel.ts
+++ src/modules/books/get-books/ui/get-books/get-books-by-name.viewmodel.ts
@@ -4,7 +4,7 @@ import { Book } from '../../connector-to.get-books';
 const selectState = (state: RootState) => ( state.catalog.getBooks.books );
 export const getBooksByNameViewmodel = (query: string) => (state: RootState) => {
   const booksState = selectState(state);
-  console.log(booksState)
+  console.log('books state', booksState)
   return booksState.filter((book: Book) => 
     book.title && book.title.toLowerCase().includes(query.toLowerCase())
 );
--- src/modules/books/get-books/ui/get-books/get-books-by-subject.utils.ts
+++ src/modules/books/get-books/ui/get-books/get-books-by-subject.utils.ts
@@ -2,6 +2,6 @@ import { Book } from '../../connector-to.get-books';
 
 export const getBooksBySubjectUtils = (booksList: Book[], subject: string) => {
     return booksList.filter((book) => {
-        return book.subject && book.subject.subject.toLowerCase().includes(subject.toLowerCase());
+        return book.subject && book.subject.subject && book.subject.subject.toLowerCase().includes(subject.toLowerCase());
     });
 }
