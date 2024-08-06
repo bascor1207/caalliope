5b2c60ea83ef31fb0a2540615a63fce4d631a132
laurabjn <92058813+laurabjn@users.noreply.github.com>
2024-08-06 21:17:50 +0200

    refacto: book info page

--- src/app/book-info/page.tsx
+++ src/app/book-info/page.tsx
@@ -1,13 +1,11 @@
 'use client'
 
 import { BookInfo } from '@/modules/books/get-one-book/ui/components/Book-info';
-import { TabBookInfo } from '@/modules/books/get-one-book/ui/components/tab-book-info';
 
 const BookInfoPage = () => {
   return (
     <div>
       <BookInfo />
-      <TabBookInfo />
     </div>
   );
 };
