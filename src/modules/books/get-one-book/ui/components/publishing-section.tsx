08c7d5d415358aa4484c9c9bf7549beca492f42e
laurabjn <92058813+laurabjn@users.noreply.github.com>
2024-08-22 22:42:18 +0200

    refacto: review section and publishing with next ui

--- src/modules/books/get-one-book/ui/components/publishing-section.tsx
+++ src/modules/books/get-one-book/ui/components/publishing-section.tsx
@@ -2,7 +2,6 @@ import { FC, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Button, Select, SelectItem, Card, CardBody } from '@nextui-org/react';
 
-import styles from './publishing-section.module.scss';
 import { CustomModal } from '@/modules/app/ui/component-level/custom.modal';
 import { BooksModel } from '@/modules/books/model/books.model';
 import { AddPublisherForm } from '@/modules/books/get-books/ui/forms/add-publisher-form';
@@ -25,34 +24,35 @@ export const PublishingSection: FC<Props> = ({ book }) => {
 
     return (
         <div>
-            <div className='flex items-start justify-between mb-4'>
-                {book.publishers.map((publisher) => (
-                    <Card key={publisher.id} className='bg-transparent text-black max-w-md flex-grow'>
-                        <CardBody className='text-black'>
-                            <span className='block mb-4'>
-                                {publisher.dateOfPublication} - {publisher.label} | {publisher.language} | {publisher.numberOfPages} pages
-                            </span>
-                            <div className='flex items-center'>
-                                <Select placeholder={t('selectOption')} className='text-black bg-red"'>
-                                    <SelectItem value='notOwned' key={0}>{t('notOwned')}</SelectItem>
-                                    <SelectItem value='inProgress' key={1}>{t('inProgress')}</SelectItem>
-                                    <SelectItem value='toRead' key={2}>{t('toRead')}</SelectItem>
-                                    <SelectItem value='read' key={3}>{t('read')}</SelectItem>
-                                    <SelectItem value='wishlist' key={4}>{t('whislist')}</SelectItem>
-                                    <SelectItem value='giveUp' key={5}>{t('giveUp')}</SelectItem>
-                                </Select>
-                                <Button onClick={handleClick} className='ml-4 bg-[#D9D9D9] hover:bg-[#f8e9ff] text-black'>
-                                    {t('valid')}
-                                </Button>
-                            </div>
-                        </CardBody>
-                    </Card>
-                ))}
-                <Button onClick={toggle} className='bg-transparent hover:bg-[#f8e9ff] text-black underline'>
+            <div className='flex justify-end'>
+                <Button onClick={toggle} className='bg-transparent hover:bg-[#f8e9ff] text-black'>
                     {t('library.addPublishing')}
                 </Button>
             </div>
+            {book.publishers.map((publisher) => (
+                <Card key={publisher.id} className='bg-transparent bg-transparent text-black'>
+                    <CardBody className='text-black'>
+                        <span>
+                            {publisher.dateOfPublication} - {publisher.label} | {publisher.language} | {publisher.numberOfPages} pages
+                        </span>
+                        <div className='flex items-center'>
+                            <Select placeholder={t('selectOption')}>
+                                <SelectItem value='notOwned' key={0}>{t('notOwned')}</SelectItem>
+                                <SelectItem value='inProgress' key={1}>{t('inProgress')}</SelectItem>
+                                <SelectItem value='toRead' key={2}>{t('toRead')}</SelectItem>
+                                <SelectItem value='read' key={3}>{t('read')}</SelectItem>
+                                <SelectItem value='wishlist' key={4}>{t('whislist')}</SelectItem>
+                                <SelectItem value='giveUp' key={5}>{t('giveUp')}</SelectItem>
+                            </Select>
+                            <Button onClick={handleClick} className='ml-4 bg-[#D9D9D9] hover:bg-[#f8e9ff] text-black'>
+                                {t('valid')}
+                            </Button>
+                        </div>
+                    </CardBody>
+                </Card>
+            ))}
             <CustomModal isShown={isShown} hideModal={toggle} modalContent={<AddPublisherForm />} />
         </div>
+
     );
 };
