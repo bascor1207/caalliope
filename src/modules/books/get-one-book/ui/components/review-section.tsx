339dd3a859bed453a00a5500823b978c9da0bc80
laurabjn <92058813+laurabjn@users.noreply.github.com>
2024-08-25 21:12:02 +0200

    style: component in book page

--- src/modules/books/get-one-book/ui/components/review-section.tsx
+++ src/modules/books/get-one-book/ui/components/review-section.tsx
@@ -20,28 +20,28 @@ export const ReviewSection: FC<Props> = ({ book }) => {
 
     return (
       <div>
-        <div className='flex justify-end'>
+        <div className='flex items-start justify-between mb-4'>
+          {book.reviews?.map((review) => (
+            <Card className='max-w-[340px] mt-2 bg-transparent text-black max-w-md flex-grow' key={review.id}>
+              <CardHeader className='justify-between'>
+                <div className='flex gap-5'>
+                  <Avatar isBordered radius='full' size='md' src='https://nextui.org/avatars/avatar-1.png' />
+                  <div className='flex flex-col gap-1 items-start justify-center'>
+                    <h4 className='font-semibold leading-none text-black'>Nom prénom</h4>
+                  </div>
+                </div>
+              </CardHeader>
+              <CardBody className='px-3 py-0 text-black'>
+                <p className='mb-4'>
+                  {review.comment}
+                </p>
+              </CardBody>
+            </Card>
+          ))}
           <Button onClick={toggle} className='bg-transparent hover:bg-[#f8e9ff] text-black mb-4'>
             {t('library.addReview')}
           </Button>
         </div>
-        {book.reviews?.map((review) => (
-          <Card className='max-w-[340px] mt-2 bg-transparent' key={review.id}>
-            <CardHeader className='justify-between'>
-              <div className='flex gap-5'>
-                <Avatar isBordered radius='full' size='md' src='https://nextui.org/avatars/avatar-1.png' />
-                <div className='flex flex-col gap-1 items-start justify-center'>
-                  <h4 className='font-semibold leading-none text-black'>Nom prénom</h4>
-                </div>
-              </div>
-            </CardHeader>
-            <CardBody className='px-3 py-0 text-black'>
-              <p>
-                {review.comment}
-              </p>
-            </CardBody>
-          </Card>
-        ))}
         <CustomModal isShown={isShown} hideModal={toggle} modalContent={<AddReviewForm />} />
       </div>
   );
