fcfc84bee5bf3c58e29ce52413bee6e5d661d917
laurabjn <92058813+laurabjn@users.noreply.github.com>
2024-08-25 21:12:02 +0200

    style: component in book page

--- src/modules/books/get-one-book/ui/components/tab-book-info.tsx
+++ src/modules/books/get-one-book/ui/components/tab-book-info.tsx
@@ -17,13 +17,16 @@ const SUBJECTS_TAB = [
 const classNames = {
   tab: 'text-custom-dark-purple',
   cursor: 'cursor-pointer',
+  tabList: 'flex flex-col w-48', 
+  tabContent: 'w-full',
+  tabPanel: 'w-full',
 };
 
 export const TabBookInfo: FC<Props> = ({ book }) => {
   const [selectedTab, setSelectedTab] = useState(SUBJECTS_TAB[0].value);
 
   return (
-    <div>
+    <div className='flex'>
       <Tabs
         aria-label='Book Info Tabs'
         selectedKey={selectedTab}
@@ -37,10 +40,11 @@ export const TabBookInfo: FC<Props> = ({ book }) => {
             key={tab.id}
             title={tab.label}
             value={tab.value}
+            className='h-16 flex items-center justify-center'
           />
         ))}
       </Tabs>
-      <div className='mt-8'>
+      <div className='ml-8 w-full'>
         {selectedTab === '0' && <PublishingSection book={book} />}
         {selectedTab === '1' && <ReviewSection book={book} />}
       </div>
