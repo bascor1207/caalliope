2de208177e9590dfca6d7638fb8ed7db49a9f419
Bastien_Corré <60731967+bascor1207@users.noreply.github.com>
2024-08-27 14:20:44 +0200

    fix

--- src/modules/store/create-app-thunk.ts
+++ src/modules/store/create-app-thunk.ts
@@ -5,7 +5,7 @@ import {
   RootState,
 } from '@/modules/store/create-store'
 
-export function createAppAsyncThunk<RType = null, Payload>(
+export function createAppAsyncThunk<RType, Payload = void>(
     typePrefix: string,
     payloadCreator: AsyncThunkPayloadCreator<RType, Payload, {
         state: RootState;
