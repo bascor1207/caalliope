e8a024df5c687ae36b1cac1581b955dd1f727e22
laurabjn <92058813+laurabjn@users.noreply.github.com>
2024-08-30 23:15:04 +0200

    feat: usecase and test add book in user library + add method in component

--- src/modules/user/get-users/usecase/get-users.usecase.ts
+++
@@ -1,8 +0,0 @@
-import { createAppAsyncThunk } from '@/modules/store/create-app-thunk';
-
-export const getUsersUseCase = createAppAsyncThunk(
-    'user/getUsers',
-    async(_, { extra: { userAdapter } }) => {
-        return await userAdapter.getUsers();
-    }
-)
