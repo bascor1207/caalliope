4b87ab5859dfaf858d43a8e55d2059222bb09644
bascor1207 <bcorre.craft@gmail.com>
2024-09-01 21:59:58 +0200

    wip for work: handle setup when creating fake create book gateway to do tommorow at work

--- src/modules/books/usecases/create-book/core/create-book.usecase.ts
+++ src/modules/books/usecases/create-book/core/create-book.usecase.ts
@@ -3,7 +3,12 @@ import { BooksModel } from '@/modules/books/model/books.model';
 
 export const createBookUsecase = createAppAsyncThunk(
     'books/create-book',
-    async (payload: BooksModel.AddBookFormSchemaType, { extra: { createBookAdapter } }) => {
-        return await createBookAdapter.create(payload)
+    async (payload: BooksModel.AddBookFormSchemaType, { rejectWithValue, extra: { createBookAdapter } }) => {
+        try {
+            return await createBookAdapter.create(payload)
+        } catch (error) {
+            console.log(error.payload);
+            return rejectWithValue(error.payload)
+        }
     }
 )
