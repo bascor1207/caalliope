a55dacc7bc86921a693dc757dd610e949b6807d9
bascor1207 <bcorre.craft@gmail.com>
2024-09-15 16:05:44 +0200

    fix build errors

--- src/modules/main.ssr.ts
+++ src/modules/main.ssr.ts
@@ -1,20 +1,21 @@
 import type { AppStore, Dependencies, RootState } from '@/modules/app/core/store/create-store';
 
 import { createStore } from '@/modules/app/core/store/create-store';
-import { BookFactory } from '@/modules/books/model/books.factory';
+// import { BookFactory } from '@/modules/books/model/books.factory';
 import { catalog } from '@/modules/catalog';
 
 // TODO UNCOMMENT THIS WHEN WANTING BACK WITH FRONT
 import { HttpCookiesProvider } from '@/modules/app/infra/http-cookies.provider';
+import { HttpAuthGateway } from '@/modules/auth/infra/http-auth.gateway';
 import { HttpGetOneBookGateway } from '@/modules/books/get-one-book/infra/http-get-one-book.gateway';
 import { HttpGetBooksGateway } from '@/modules/books/usecases/get-catalog/infra/http-get-books.gateway';
 import { HttpDonateGateway } from '@/modules/donate/infra/http-donate.gateway';
-// import { HttpUserGateway } from '@/modules/user/infra/http-user.gateway';
+import { HttpUserGateway } from '@/modules/user/infra/http-user.gateway';
 
 
 //TODO UNCOMMENT THIS WHEN WANTING ONY LOCAL
 
-import { FakeAuthGateway } from '@/modules/auth/infra/fake-auth.gateway';
+// import { FakeAuthGateway } from '@/modules/auth/infra/fake-auth.gateway';
 // import { FakeGetOneBookGateway } from '@/modules/books/get-one-book/infra/fake-get-one-book.gateway';
 import { FakeCreateBookGateway } from '@/modules/books/usecases/create-book/infra/fake-create-book.gateway';
 import { FakeCreateEditionGateway } from '@/modules/books/usecases/create-edition/infra/fake-create-edition.gateway';
@@ -23,12 +24,10 @@ import { FakeGetLastReleaseBooksGateway } from '@/modules/books/usecases/get-las
 import { FakeGetPopularBooksGateway } from '@/modules/books/usecases/get-popular-books/infra/fake-get-popular-books.gateway';
 import { FakeUpdateBookGateway } from '@/modules/books/usecases/update-book/infra/fake-update-book.gateway';
 import { FakeUpdateEditionGateway } from '@/modules/books/usecases/update-edition/infra/fake-update-edition.gateway';
-import { FakeUserGateway } from '@/modules/user/infra/fake-user.gateway';
+// import { FakeUserGateway } from '@/modules/user/infra/fake-user.gateway';
 import { FakeAdminGateway } from '@/modules/user/usecases/admin/infra/fake-admin.gateway';
 
-
-
-const book = BookFactory.create();
+// const book = BookFactory.create();
 
 export class SSRApp {
     public dependencies: Dependencies;
@@ -43,8 +42,8 @@ export class SSRApp {
 
 
         // TODO UNCOMMENT THIS WHEN WANTING BACK WITH FRONT
-        // const authAdapter = new HttpAuthGateway();
-        // const userAdapter = new HttpUserGateway();
+        const authAdapter = new HttpAuthGateway();
+        const userAdapter = new HttpUserGateway();
         // const getBooksAdapter = new HttpGetBooksGateway();
         const getOneBookAdapter = new HttpGetOneBookGateway();
         const getBooksAdapter = new HttpGetBooksGateway();
@@ -54,8 +53,8 @@ export class SSRApp {
 
 
         //TODO UNCOMMENT THIS WHEN WANTING ONY LOCAL
-        const authAdapter = new FakeAuthGateway();
-        const userAdapter = new FakeUserGateway();
+        // const authAdapter = new FakeAuthGateway();
+        // const userAdapter = new FakeUserGateway();
         const adminAdapter = new FakeAdminGateway();
         // const getOneBookAdapter = new FakeGetOneBookGateway();
         // getOneBookAdapter.returnedResponse = book;
