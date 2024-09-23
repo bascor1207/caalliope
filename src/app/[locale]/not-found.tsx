43c401dee192b733b951339dc9ed1af876f17e43
Bastien Corré <bastiencorre@my-mac.local>
2024-09-23 18:33:49 +0200

    feat: add translations and fix things

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,13 +1,19 @@
+'use client';
 import { Button, Link } from '@nextui-org/react';
+import { useTranslation } from 'react-i18next';
 
 export default function NotFound() {
+    const { t } = useTranslation();
+
     return (
         <div className='flex flex-col items-center justify-center p-4'>
-            <h1 className='text-4xl font-bold text-gray-800'>404 - Page non trouvée</h1>
+            <h1 className='text-4xl font-bold text-gray-800'>{t('notFound.title')}</h1>
             <p className='mt-2 text-lg text-gray-600'>
-                {"La page que vous cherchez n'existe pas ou a été déplacée."}
+                {t('notFound.description')}
             </p>
-            <Button as={Link} href={'/'} className='bg-custom-purple m-6'> {"Retour à l'accueil"}</Button>
+            <Button as={Link} href={'/'} className='bg-custom-purple m-6'>
+                {t('notFound.backHome')}
+            </Button>
         </div>
     );
 }
