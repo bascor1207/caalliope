9385c2473f7978bf506c4a8fbf350f03b2462464
Bastien Corré <bastiencorre@my-mac.local>
2024-09-23 15:24:26 +0200

    feat: implement get search

--- src/modules/books/usecases/get-catalog/core/connector-to.get-books.ts
+++ src/modules/books/usecases/get-catalog/core/connector-to.get-books.ts
@@ -3,6 +3,6 @@ import type { BooksModel } from '@/modules/books/model/books.model';
 export interface ConnectorToGetBooks {
     getBooks(): Promise<BooksModel.BookForCatalog[] | void>;
     getBooksByGenre(value: string) : Promise<BooksModel.BookForCatalog[] | void>;
-    getBooksByAuthor(value: string) : Promise<BooksModel.BookForCatalog[]>;
-    getBooksByName(value: string) : Promise<BooksModel.BookForCatalog[]>;
+    getBooksByAuthor(value: string) : Promise<BooksModel.BookForCatalog[] | void>;
+    getBooksByName(value: string) : Promise<BooksModel.BookForCatalog[] | void>;
 }
