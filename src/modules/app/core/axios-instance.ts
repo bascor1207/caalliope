b8ebbe951fc5968b037cbbe0f0b14fc57a9053c8
Bastien Corré <bastiencorre@my-mac.local>
2024-09-25 13:53:43 +0200

    feature: add env key to local or production rewrites

--- src/modules/app/core/axios-instance.ts
+++ src/modules/app/core/axios-instance.ts
@@ -5,7 +5,7 @@ import { HttpCookiesProvider } from '@/modules/app/infra/http-cookies.provider';
 const isServer = typeof window === 'undefined'
 
 export const axiosInstance = axios.create({
-    baseURL: 'https://caalliope.vercel.app/api',
+    baseURL: `${process.env.NEXT_PUBLIC_DEFAULT_URL}/api`,
     withCredentials: true
 });
 
